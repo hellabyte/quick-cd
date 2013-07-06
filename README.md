716c0cba27d5b4c5b17c82154cf6e6c3ccfdea21
Nathaniel Hellabyte <nate@hellabit.es>
2013-07-06 00:12:45 -0700

    :)

--- README.md
+++ README.md
@@ -14,13 +14,25 @@ Installation
 ============
 
 Note that qcd must reside sourced in the current enviroment to be of use.
-It is recommended to do the following with a tar.gz until an installer is in place:
+For Mac OS X, it is recommended to do the following with a
+ tar.gz until a formal installer is in place:
 
 tar xzvf quick-cd.tar.gz /usr/local/lib/quick-cd  
 cd /usr/local/lib/quick-cd  
-sudo bash install.bash  
+bash install.bash  
+
+For Linux, follow the same steps. You will be prompted for a password during install.
+This password gives the program needed sudo access.
 
 You may want to remove the tarball after statisifed that the package installed correctly.
+
+It is also possible to use git:
+
+git clone https://github.com/hellabyte/quick-cd.git  
+cd ./quick-cd  
+bash install.bash
+
+You can remove the directory that git clone creates after a successful install.  
 Otherwise, enjoy qcd and try not to get too spoiled!
     
 Usage
@@ -47,4 +59,4 @@ Desired Future Modifications
 Make sure that any updates to the program have the version date updated in the functions.bash
 file as well (4th comment line).
 
-(C) 2013 - July - 05 Nathaniel Hellabyte nate@hellabit.es
+(C) 2013 - July - 06 Nathaniel Hellabyte nate@hellabit.es
--- install.bash
+++ install.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# (C) 2013 - July - 05
+# (C) 2013 - July - 06
 # Nathaniel Hellabyte
 # nate@hellabit.es
 # RECOMMENDED USAGE
@@ -12,7 +12,6 @@
 # CHECKS   before overwrite
 # BACKUPS  $FUNCTIONS_TARGET 
 # = = = = = = = = = = = = = =-
-
 USER_HOME=$(eval echo ~${SUDO_USER})
 PROGRAM_RAW="$(pwd)/quick-cd.bash"
 FUNCTIONS_RAW="$(pwd)/functions.bash"
@@ -25,8 +24,9 @@ FUNCTIONS_SOURCE_TARGET="${USER_HOME}/.bashrc"
 FUNCTIONS_TARGET_BACKUP="${NEW_HOME_DIR}/.backups"
 FUNCTIONS_TARGET="${FUNCTIONS_TARGET_BACKUP}/.supporting_rc.bash"
 FUNCTIONS_TEMP="${FUNCTIONS_TARGET_BACKUP}/.supporting_rc_temp"
+FUNCTIONS_SOURCE_SUPPORT="${USER_HOME}/.bash_profile"
 
-[[ -f $PROGRAM_RAW ]] || ( echo "Please change into directory containing install files" >&2 && exit 3 )
+[[ -f $PROGRAM_RAW ]] || ( echo "Please change into directory containing install files" >&2 && exit 29 )
 
 if [ ! -d $NEW_HOME_DIR ]; then
     mkdir -p $NEW_HOME_DIR
@@ -38,49 +38,81 @@ fi
 touch $GENERAL_DIR $QUERIED_DIR
 cp $FUNCTIONS_SOURCE_TARGET "${FUNCTIONS_TARGET_BACKUP}"
 
-if [ -f $PROGRAM_TARGET_LIB ]; then
-    echo "WARNING--${PROGRAM_TARGET_LIB} ALREADY EXISTS."
-    while true; do
-        read -p "overwrite? [y/n]: " OVERWRITE_DECISION || exit
-        if [ "$OVERWRITE_DECISION" == 'y' ]; then
-            rm $PROGRAM_TARGET_LIB
-            break
-        else
-            echo "Choosing to quit because of overwrite potential."
-            exit 1
-        fi
-    done
-fi
-
-if [ -f $FUNCTIONS_SOURCE_TARGET ]; then
-    BEG_FLAG="# BEGIN QUICK-CD FUNCTIONS"
-    END_FLAG="# END QUICK-CD FUNCTIONS"
-    BEG_FLAG_LINE_NUMBER=$(grep -n "$BEG_FLAG" $FUNCTIONS_SOURCE_TARGET | cut -f 1 -d ':')
-    END_FLAG_LINE_NUMBER=$(grep -n "$END_FLAG" $FUNCTIONS_SOURCE_TARGET | cut -f 1 -d ':')
-    read -a BLN -d ' ' < <(echo "${BEG_FLAG_LINE_NUMBER[@]}")
-    read -a ELN -d ' ' < <(echo "${END_FLAG_LINE_NUMBER[@]}")
-    BIN=0; EIN=$((${#ELN[@]} - 1))
-    if [ ${#BLN[@]} -eq ${#ELN[@]} ]; then
-        if [ $EIN -ne -1 ]; then
-            sed "${BLN[$BIN]},${ELN[$EIN]}d" $FUNCTIONS_SOURCE_TARGET > $FUNCTIONS_TEMP
-            mv $FUNCTIONS_TEMP $FUNCTIONS_SOURCE_TARGET
-        fi
-    else
-        echo "WARNING -- Manual tidying of $FUNCTIONS_SOURCE_TARGET required." >&2
+function installer{
+    if [ -f $PROGRAM_TARGET_LIB ]; then
+        echo "WARNING--${PROGRAM_TARGET_LIB} ALREADY EXISTS."
+        while true; do
+            read -p "overwrite? [y/n]: " OVERWRITE_DECISION || exit
+            if [ "$OVERWRITE_DECISION" == 'y' ]; then
+                rm $PROGRAM_TARGET_LIB
+                break
+            else
+                echo "Choosing to quit because of overwrite potential."
+                exit 1
+            fi
+        done
     fi
-fi
 
-cp $PROGRAM_RAW $PROGRAM_TARGET_LIB || exit 72
-chmod 744 $PROGRAM_TARGET_LIB || exit 73
-if [ -f $PROGRAM_TARGET_BIN ]; then
-    rm $PROGRAM_TARGET_BIN | exit 75
-fi
-ln -s $PROGRAM_TARGET_LIB $PROGRAM_TARGET_BIN || exit 77
-cp $FUNCTIONS_RAW $FUNCTIONS_TARGET || exit 78
-cat << EOF >> $FUNCTIONS_SOURCE_TARGET 
+    if [ -f $FUNCTIONS_SOURCE_TARGET ]; then
+        BEG_FLAG="# BEGIN QUICK-CD FUNCTIONS"
+        END_FLAG="# END QUICK-CD FUNCTIONS"
+        BEG_FLAG_LINE_NUMBER=$(grep -n "$BEG_FLAG" $FUNCTIONS_SOURCE_TARGET | cut -f 1 -d ':')
+        END_FLAG_LINE_NUMBER=$(grep -n "$END_FLAG" $FUNCTIONS_SOURCE_TARGET | cut -f 1 -d ':')
+        read -a BLN -d ' ' < <(echo "${BEG_FLAG_LINE_NUMBER[@]}")
+        read -a ELN -d ' ' < <(echo "${END_FLAG_LINE_NUMBER[@]}")
+        BIN=0; EIN=$((${#ELN[@]} - 1))
+        if [ ${#BLN[@]} -eq ${#ELN[@]} ]; then
+            if [ $EIN -ne -1 ]; then
+                sed "${BLN[$BIN]},${ELN[$EIN]}d" $FUNCTIONS_SOURCE_TARGET > $FUNCTIONS_TEMP
+                mv $FUNCTIONS_TEMP $FUNCTIONS_SOURCE_TARGET
+            fi
+        else
+            echo "WARNING -- Manual tidying of $FUNCTIONS_SOURCE_TARGET required." >&2
+        fi
+    fi
+
+    cp $PROGRAM_RAW $PROGRAM_TARGET_LIB || exit 72
+    chmod 755 $PROGRAM_TARGET_LIB || exit 73
+    [[ -f $PROGRAM_TARGET_BIN ]] && ( rm $PROGRAM_TARGET_BIN || exit 75 )
+    ln -s $PROGRAM_TARGET_LIB $PROGRAM_TARGET_BIN || exit 77
+    cp $FUNCTIONS_RAW $FUNCTIONS_TARGET || exit 78
+    cat << EOF >> $FUNCTIONS_SOURCE_TARGET 
 # BEGIN QUICK-CD FUNCTIONS
 # DO NOT DELETE ABOVE COMMENT
 [[ -f "$FUNCTIONS_TARGET" ]] && builtin source "$FUNCTIONS_TARGET"
 # DO NOT DELETE BELOW COMMENT
 # END QUICK-CD FUNCTIONS
 EOF
+    builtin source $FUNCTIONS_SOURCE_TARGET
+    echo "Installation complete."
+}
+
+function function_support_linker {
+ cat << EOF >> $FUNCTIONS_SOURCE_SUPPORT   
+# BEGIN QUICK-CD SUPPORT -- DO NOT DELETE
+[[ -f "$FUNCTIONS_SOURCE_TARGET" ]] && builtin source "$FUNCTIONS_SOURCE_TARGET"
+# END QUICK-CD SUPPORT -- DO NOT DELETE
+EOF
+}
+
+echo "quick-cd resides as an alias in ${FUNCTIONS_SOURCE_TARGET}."
+echo "In order to work properly, ${FUNCTIONS_SOURCE_TARGET} must"
+echo "be sourced by the files sourced by BASH environments."
+
+if [ ! -z $OSTYPE ]; then
+    if [ $OSTYPE == "darwin12" ]; then
+        echo "It is recommended to install without sudo."
+        if [ ! -f $FUNCTIONS_SOURCE_SUPPORT ]; then
+            touch $FUNCTIONS_SOURCE_SUPPORT
+            function_support_linker
+        fi
+        installer
+    elif [ $OSTYPE == "gnu-linux" ]; then
+        echo "It is recommended to install with sudo."
+        sudo -p "${USER}, please provide your password to allow sudo: " installer
+    else
+        echo "Operating System unrecognized." >&2 && exit 96
+    fi
+fi
+
+echo 'Installation complete!'
